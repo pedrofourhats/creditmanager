16a5ed1cc5cbfe3cefa846deb2fc7935f942a946
davidfrias01 <davidfrias01@gmail.com>
2014-12-15 01:50:21 +0000

    Se implementa el nuevo pager

--- creditmanager/src/main/java/com/creditmanager/dao/ProjectDAO.java
+++ creditmanager/src/main/java/com/creditmanager/dao/ProjectDAO.java
@@ -1,7 +1,8 @@
 package com.creditmanager.dao;
 
+import com.creditmanager.model.Page;
 import com.creditmanager.model.Project;
 
 public interface ProjectDAO extends GenericDAO<Project, Long> {
-
+	Page<Project> getAllProjects(int pageIndex, int pageSize);
 }
---
+++ creditmanager/src/main/java/com/creditmanager/model/Page.java
@@ -0,0 +1,66 @@
+package com.creditmanager.model;
+
+import java.util.List;
+
+public class Page<T> {
+	
+	private List<T> elements;
+	
+	private int pageIndex;
+	
+	private int pageSize;
+	
+	private Integer totalItems;
+
+	public Page(List<T> elements, int pageIndex, int pageSize, Integer totalItems){
+		this.elements = elements;
+		this.pageIndex = pageIndex;
+		this.pageSize = pageSize;
+		this.totalItems = totalItems;
+	}
+	
+	public List<T> getElements() {
+		return elements;
+	}
+
+	public void setElements(List<T> elements) {
+		this.elements = elements;
+	}
+
+	public int getPageIndex() {
+		return pageIndex;
+	}
+
+	public void setPageIndex(int pageIndex) {
+		this.pageIndex = pageIndex;
+	}
+
+	public int getPageSize() {
+		return pageSize;
+	}
+
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	public int getTotalItems() {
+		return totalItems;
+	}
+
+	public void setTotalItems(Integer totalItems) {
+		this.totalItems = totalItems;
+	}
+
+	public int getTotalPages() {
+		if(totalItems == 0){
+			return 1;
+		}
+		
+		int pages = (totalItems / pageSize) + 1;
+		if(totalItems % pageSize == 0){
+			return pages - 1;
+		} else {
+			return pages;
+		}
+	}
+}
