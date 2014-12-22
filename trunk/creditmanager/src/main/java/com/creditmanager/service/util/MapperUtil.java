1be0b9600e122147e17db5159c9beae082845643
danielorozco87@gmail.com <danielorozco87@gmail.com@49c86286-db30-99ec-4c58-f7b3ce1121b1>
2014-12-22 02:15:19 +0000

    bugfixing y search en personas

--- trunk/creditmanager/src/main/java/com/creditmanager/service/util/MapperUtil.java
+++ trunk/creditmanager/src/main/java/com/creditmanager/service/util/MapperUtil.java
@@ -20,4 +20,8 @@ public class MapperUtil {
 		final List<U> elements = map(mapper, source.getElements(), destType);
 		return new Page<U>(elements, source.getPageIndex(), source.getPageSize(), source.getTotalItems());
 	}
+	
+	public static <T, U> U map(final Mapper mapper, final T source, final Class<U> destType){
+		return mapper.map(source, destType);
+	}
 }
--- trunk/creditmanager/src/main/webapp/static/scripts/viewmodels/persons/personsList.js
+++ trunk/creditmanager/src/main/webapp/static/scripts/viewmodels/persons/personsList.js
@@ -2,6 +2,7 @@ var personsListApp = angular.module('personApp', []);
 
 personsListApp.controller('personsListController', function ($scope, $http) {
     $scope.personsPage = personsPage;
+    $scope.isSearching = false;
 	
     $scope.deletePerson = function(person) {
     	var confirmDeletion = confirm("¿Está seguro que desea eliminar esta persona?");
@@ -16,9 +17,58 @@ personsListApp.controller('personsListController', function ($scope, $http) {
     	}
     };
     
+    $scope.goToEditPerson = function(person) {
+    	redirect('person/edit/' + person.id);
+    };
+    
 	$scope.goToPage = function(pageNumber) {
 		if($scope.personsPage.pageIndex != pageNumber) {
-			$http.get(getCompletePath("persons/" + pageNumber + "/10"))
+			var path = "persons/" + pageNumber + "/10" + ($scope.searchedKeyword ? "/" + $scope.searchedKeyword : "");
+			$http.get(getCompletePath(path))
+			.success(function (newPersonsPage) {
+				newPersonsPage.pagesToShow = function () {
+			        var pages = [];
+			        var firstPage;
+			        var lastPage;
+	
+			        if (newPersonsPage.totalPages < 8) {
+			            firstPage = 1;
+			            lastPage = newPersonsPage.totalPages;
+			        } else if (newPersonsPage.pageIndex < 4) {
+			            firstPage = 1;
+			            lastPage = 7;
+			        } else if (newPersonsPage.pageIndex > newPersonsPage.totalPages - 3) {
+			            lastPage = newPersonsPage.totalPages;
+			            firstPage = lastPage - 6;
+			        } else {
+			            firstPage = newPersonsPage.pageIndex - 3;
+			            lastPage = newPersonsPage.pageIndex + 3;
+			        }
+	
+			        for (var i = firstPage; i <= lastPage; i++) {
+			            pages.push(i);
+			        }
+	
+			        return pages;
+			    };
+				$scope.personsPage = newPersonsPage;
+		    }).error(function () {
+		    	alert("Ha ocurrido un problema. Por favor intente nuevamente");
+		    });
+		}
+	};
+	
+	$scope.deleteSearch = function() {
+		$scope.isSearching = false;
+		$scope.searchedKeyword = "";
+		$scope.goToPage(0);
+	};
+	
+	$scope.search = function() {
+		if($scope.searchedKeyword) {
+			$scope.isSearching = true;
+			
+			$http.get(getCompletePath("persons/1/10/" + $scope.searchedKeyword))
 			.success(function (newPersonsPage) {
 				newPersonsPage.pagesToShow = function () {
 			        var pages = [];
