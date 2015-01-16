488405a8f6f6247669fe86b6466a4aac2353f6c3
pedroalessandri <pedroalessandri@gmail.com>
2015-01-16 19:53:03 +0000

    reportes - templates a modificar copiados de defaulters.jps ReportController tiene los controladores
    Cambio y fuera.

--- creditmanager/src/main/java/com/creditmanager/controller/ReportController.java
+++ creditmanager/src/main/java/com/creditmanager/controller/ReportController.java
@@ -16,4 +16,57 @@ public class ReportController {
     public String goToDefaulters(Model model) {
         return "content/report/defaulters";
     }
+	
+	@RequestMapping(value="/report/datosFiliatoriosTitulares")
+    public String goToDatosFiliatoriosTitulares(Model model) {
+        return "content/report/datosFiliatoriosTitulares";
+    }
+	
+	@RequestMapping(value="/report/reporteDeMora")
+    public String goToReporteDeMora(Model model) {
+        return "content/report/reporteDeMora";
+    }
+	
+	@RequestMapping(value="/report/carteraDeCreditosActivos")
+    public String goToCarteraDeCreditosActivos(Model model) {
+        return "content/report/carteraDeCreditosActivos";
+    }
+	
+	@RequestMapping(value="/report/indicadoresSocioEconomicos")
+    public String goToIndicadoresSocioEconomicos(Model model) {
+        return "content/report/indicadoresSocioEconomicos";
+    }
+	
+	@RequestMapping(value="/report/registroDePagos")
+    public String goToRegistroDePagos(Model model) {
+        return "content/report/registroDePagos";
+    }
+	
+	@RequestMapping(value="/report/movimientos")
+    public String goToMovimientos(Model model) {
+        return "content/report/movimientos";
+    }
+	
+	@RequestMapping(value="/report/creditosOtorgados")
+    public String goToCreditosOtorgados(Model model) {
+        return "content/report/creditosOtorgados";
+    }
+	
+	@RequestMapping(value="/report/cobranzas")
+    public String goToCobranzas(Model model) {
+        return "content/report/cobranzas";
+    }
+	
+	@RequestMapping(value="/report/estadoDeCartera")
+    public String goToEstadoDeCartera(Model model) {
+        return "content/report/estadoDeCartera";
+    }
+	
+	@RequestMapping(value="/report/movimientoDeBanco")
+    public String goToMovimientoDeBanco(Model model) {
+        return "content/report/movimientoDeBanco";
+    }
+	
+
+	
 }
