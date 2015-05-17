143603fda355df52af9cef91f40770864be2d02e
davidfrias01 <davidfrias01@gmail.com>
2015-05-17 21:09:24 +0000

    bugfixing de search de proyectos y dni

--- creditmanager/src/main/java/com/creditmanager/dao/impl/ProjectDAOImpl.java
+++ creditmanager/src/main/java/com/creditmanager/dao/impl/ProjectDAOImpl.java
@@ -1,5 +1,9 @@
 package com.creditmanager.dao.impl;
 
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.criterion.DetachedCriteria;
 import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Restrictions;
@@ -26,16 +30,31 @@ public class ProjectDAOImpl extends GenericDAOImpl<Project, Long> implements Pro
 
 	@Override
 	public Page<Project> findByProjectNumber(int pageIndex, int pageSize, String number) {
-		DetachedCriteria criteria = DetachedCriteria.forEntityName(entityName);
-		criteria.add(
-			Restrictions.or(
-				Restrictions.or(
-					Restrictions.ilike("title", number, MatchMode.ANYWHERE),
-					Restrictions.ilike("investmentDestination", number, MatchMode.ANYWHERE)
-				),
-				Restrictions.ilike("number", number, MatchMode.ANYWHERE)
-			)
-		);
-		return getPageByCriteria(criteria, pageIndex, pageSize);
+//		DetachedCriteria criteria = DetachedCriteria.forEntityName(entityName);
+//		criteria.add(
+//			Restrictions.or(
+//				Restrictions.or(
+//					Restrictions.ilike("title", number, MatchMode.ANYWHERE),
+//					Restrictions.ilike("investmentDestination", number, MatchMode.ANYWHERE)
+//				),
+//				Restrictions.ilike("number", number, MatchMode.ANYWHERE)
+//			)
+//		);
+//		return getPageByCriteria(criteria, pageIndex, pageSize);
+		
+		String countQueryString = "Select count(*) from " + entityName;
+		String selectQueryString = "From " + entityName;
+		String whereQueryString = " where " + "title like '%" + number + "%' or investmentDestination like '%" + number + "%' or number like '%" + number + "%'";
+		
+		Session session = hibernateTemplate.getSessionFactory().getCurrentSession();
+		Query countQuery = session.createQuery(countQueryString + whereQueryString);
+		Long totalItems = (Long) countQuery.uniqueResult();
+		
+		Query selectQuery = session.createQuery(selectQueryString + whereQueryString);
+		selectQuery.setFirstResult((pageIndex - 1) * pageSize)
+			.setMaxResults(pageSize);
+		
+		List<Project> elements = (List<Project>) selectQuery.list();
+		return new Page<Project>(elements, pageIndex, pageSize, totalItems);
 	}
 }
