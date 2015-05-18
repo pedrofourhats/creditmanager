60965c10108e57a70d5003f0363e73a27a551ca0
pedroalessandri <pedroalessandri@gmail.com>
2015-05-18 02:16:48 +0000

    Detalles en project

--- creditmanager/src/main/webapp/static/scripts/viewmodels/projects/projectCreation.js
+++ creditmanager/src/main/webapp/static/scripts/viewmodels/projects/projectCreation.js
@@ -196,15 +196,19 @@ projectControllers.controller('ProjectCreationCtrl', ['$scope','$http', '$modal'
 				$scope.project.number = $scope.project.numberId + '/' + $scope.project.numberYear;
 			}
 			
+			if(($scope.project.dateOfEntry.getFullYear() != new Date().getFullYear()) || ($scope.project.dateOfEntry.getDay() != new Date().getDay()) || ($scope.project.dateOfEntry.getMonth() != new Date().getMonth())) {
+				if($scope.project.dateOfEntry > new Date()) {
+					alert("La fecha de ingreso debe ser igual o inferior a la fecha de hoy");
+					return; 
+				}
+			}
+			
+			
 			if($scope.project.deliveryDate) {
 				if($scope.project.deliveryDate > new Date()) {
 					alert("La fecha de otorgamiento del cr\u00e9dito debe ser menor que la fecha actual");
 					return;
 				} 
-				else if($scope.project.dateOfEntry <= new Date()) {
-					alert("La fecha de ingreso debe ser menor o igual a la fecha de hoy");
-					return; 
-				}
 				else if(!$scope.project.dateOfEntry) {
 					alert("Para ingresar la fecha de otorgamiento del cr\u00e9dito debe primero ingresar la fecha de ingreso al INTI");
 					return;
